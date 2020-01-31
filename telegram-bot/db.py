d27f51774d0ad380cf397b0d02e4ba6d0f1cd7f0
blastz13 <blastz13@mail.ru>
2020-02-01 00:24:58 +0300

    create /timetable, work with bd

--- telegram-bot/db.py
+++ telegram-bot/db.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Tuple
 import sqlite3
 
 
-conn = sqlite3.connect(os.path.join("db", "finance.db",), check_same_thread = False)
+conn = sqlite3.connect("db/finance.db", check_same_thread = False)
 cursor = conn.cursor()
 
 
@@ -61,3 +61,47 @@ def check_db_exists():
     _init_db()
 
 check_db_exists()
+# # insert("lessons", {"dayweek":"Вторник","lesson":"Химия","homework":"Параграф 3"})
+
+def fetch_timetable_dayweek(table: str, columns: str) -> List[Tuple]:
+    """Вisplays the schedule for the day of the week"""
+    columns_joined = ", ".join(columns)
+    cursor.execute(f"SELECT * FROM {table} WHERE dayweek LIKE '{columns}%'")
+    rows = cursor.fetchall()
+    
+    return rows
+
+
+def add_timetable_homework(table: str, dayweek: str, num: str, homework: str):
+    """Add homework"""
+    cursor.execute(f"UPDATE '{table}' SET homework='{homework}' WHERE num='{num}' and dayweek='{dayweek}'")
+    rows = conn.commit()
+    
+    return rows
+
+def delete_timetable_lesson(table: str, dayweek: str, num: str):
+    """Remove lesson from timetable"""
+    cursor.execute(f"DELETE FROM '{table}' WHERE dayweek='{dayweek}' AND num='{num}'")
+    rows = conn.commit()
+    
+    return rows   
+
+
+def create_timetable_lesson(table: str, column_values: Dict):
+    """Add lesson in timetable"""
+    columns = ', '.join( column_values.keys() )
+    values = [tuple(column_values.values())]
+    placeholders = ", ".join( "?" * len(column_values.keys()) )
+    cursor.executemany(
+        f"INSERT INTO {table} "
+        f"({columns}) "
+        f"VALUES ({placeholders})",
+        values)
+    conn.commit()
+
+def edit_timetable_lesson(table: str, dayweek: str, num: int, lesson: str, homework=""):
+    """Edits a lesson"""
+    cursor.execute(f"UPDATE '{table}' SET homework='{homework}', lesson='{lesson}', homework='{homework}' WHERE num='{num}' and dayweek='{dayweek}'")
+    rows = conn.commit()
+    
+    return rows
