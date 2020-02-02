aa3008fa2677beac616ead04fa99c459767930d7
blastz13 <sinicinr2003@mail.ru>
2020-02-02 19:38:24 +0300

    fixed output.py

--- telegram-bot/output.py
+++ telegram-bot/output.py
@@ -1,18 +1,20 @@
 import timesheet
 row=""
 
-# homework= timesheet.get_tomorow_homework()
+# homework= timesheet.get_today_homework()
+# homework.sort(key=lambda a: a[1])
 # for i in range(0, len(homework)):
 #     if i == 0:
-#         row=f"--------------\n{homework[i][0]}\n{homework[i][1]}\n{homework[i][2]}: {homework[i][3]}\n"
+#         row=f"--------------\n{homework[i][0]}\n{homework[i][1]}.{homework[i][2]}: {homework[i][3]}\n"
 #         continue
 #     elif homework[i][0] != homework[i-1][0]:
-#         row+=f"--------------\n{homework[i][0]}\n{homework[i][1]}\n{homework[i][2]}: {homework[i][3]}\n"
+#         row+=f"--------------\n{homework[i][0]}\n{homework[i][1]}.\n{homework[i][2]}: {homework[i][3]}\n"
 #         continue
-#     row+=f"{homework[i][2]}: {homework[i][3]}\n"
+#     row+=f"{homework[i][1]}.{homework[i][2]}: {homework[i][3]}\n"
 # print(row)
 
 # homework= timesheet.get_week_homework()
+# print(homework)
 # for i in range(0, len(homework)):
 #     if i == 0:
 #         row=f"--------------\n{homework[i][0]}\n{homework[i][1]}\n{homework[i][2]}: {homework[i][3]}\n"
@@ -25,6 +27,7 @@ row=""
 # print(row)
 
 # homework = timesheet.get_timetable_today()
+# homework.sort(key=lambda a: a[1])
 # for i in range(0, len(homework)):
 #     if i == 0:
 #         row=f"--------------\n{homework[i][0]}\n{homework[i][1]}\n{homework[i][2]}: {homework[i][3]}\n"
@@ -36,14 +39,15 @@ row=""
 
 # print(row)
 
-homework = timesheet.get_all_timetable()
-for i in range(0, len(homework)):
-    if i == 0:
-        row=f"--------------\n{homework[i][0]}\n{homework[i][1]}.{homework[i][2]}\n"
-        continue
-    elif homework[i][0] != homework[i-1][0]:
-        row+=f"--------------\n{homework[i][0]}\n{homework[i][1]}.{homework[i][2]}\n"
-        continue
-    row+=f"{homework[i][1]}.{homework[i][2]}\n"
+# homework = timesheet.get_all_timetable()
+# for i in range(0, len(homework)):
+#     if i == 0:
+#         row=f"--------------\n{homework[i][0]}\n{homework[i][1]}.{homework[i][2]}\n"
+#         continue
+#     elif homework[i][0] != homework[i-1][0]:
+#         row+=f"--------------\n{homework[i][0]}\n{homework[i][1]}.{homework[i][2]}\n"
+#         continue
+#     row+=f"{homework[i][1]}.{homework[i][2]}\n"
 
-print(row)
+# print(row)
+
