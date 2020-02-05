15a8fcf97eeefce14d60db81313e8795d636705c
blastz13 <sinicinr2003@mail.ru>
2020-02-05 20:19:31 +0300

    control save

--- telegram-bot/config.py
+++ telegram-bot/config.py
@@ -1,4 +1,4 @@
 """API_TOKEN"""
-TOKEN = "962597549:AAHZfSdjQlCdytCmYztIixgX4hZieB1r-Yw"
+TOKEN = "1094632546:AAGu4SvzqVs9wB4t1y7X1nzzJhFc4-yasms"
 """PROXY"""
 PROXY = {'https':'socks5://94.130.35.212:1080'}
